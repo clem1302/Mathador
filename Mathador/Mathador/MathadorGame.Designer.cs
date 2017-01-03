f1b09051d5feb1e4be23f62e43b86d8b6ec5a170
RodolpheEpsi <rodolphe.bourgouin@gmail.com>
2017-01-03 09:43:22 +0100

    Add files via upload
    Commencement

---
+++ Mathador/Mathador/MathadorGame.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Mathador
+{
+    partial class MathadorGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BouttonNombre1 = new System.Windows.Forms.Button();
+            this.BouttonNombre2 = new System.Windows.Forms.Button();
+            this.BouttonNombre3 = new System.Windows.Forms.Button();
+            this.BouttonNombre4 = new System.Windows.Forms.Button();
+            this.ButtonNombre5 = new System.Windows.Forms.Button();
+            this.NombreCible = new System.Windows.Forms.Label();
+            this.TextCible = new System.Windows.Forms.Label();
+            this.TextTimer = new System.Windows.Forms.Label();
+            this.TextTemps = new System.Windows.Forms.Label();
+            this.TextAffichageScore = new System.Windows.Forms.Label();
+            this.TextScore = new System.Windows.Forms.Label();
+            this.ButtonSuivant = new System.Windows.Forms.Button();
+            this.ButtonRetour = new System.Windows.Forms.Button();
+            this.ButtonPlus = new System.Windows.Forms.Button();
+            this.ButtonMoins = new System.Windows.Forms.Button();
+            this.ButtonFois = new System.Windows.Forms.Button();
+            this.ButtonDiv = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BouttonNombre1
+            // 
+            this.BouttonNombre1.Location = new System.Drawing.Point(23, 225);
+            this.BouttonNombre1.Name = "BouttonNombre1";
+            this.BouttonNombre1.Size = new System.Drawing.Size(75, 23);
+            this.BouttonNombre1.TabIndex = 0;
+            this.BouttonNombre1.UseVisualStyleBackColor = true;
+            // 
+            // BouttonNombre2
+            // 
+            this.BouttonNombre2.Location = new System.Drawing.Point(104, 225);
+            this.BouttonNombre2.Name = "BouttonNombre2";
+            this.BouttonNombre2.Size = new System.Drawing.Size(75, 23);
+            this.BouttonNombre2.TabIndex = 1;
+            this.BouttonNombre2.UseVisualStyleBackColor = true;
+            // 
+            // BouttonNombre3
+            // 
+            this.BouttonNombre3.Location = new System.Drawing.Point(185, 225);
+            this.BouttonNombre3.Name = "BouttonNombre3";
+            this.BouttonNombre3.Size = new System.Drawing.Size(75, 23);
+            this.BouttonNombre3.TabIndex = 2;
+            this.BouttonNombre3.UseVisualStyleBackColor = true;
+            // 
+            // BouttonNombre4
+            // 
+            this.BouttonNombre4.Location = new System.Drawing.Point(266, 225);
+            this.BouttonNombre4.Name = "BouttonNombre4";
+            this.BouttonNombre4.Size = new System.Drawing.Size(75, 23);
+            this.BouttonNombre4.TabIndex = 3;
+            this.BouttonNombre4.UseVisualStyleBackColor = true;
+            // 
+            // ButtonNombre5
+            // 
+            this.ButtonNombre5.Location = new System.Drawing.Point(347, 225);
+            this.ButtonNombre5.Name = "ButtonNombre5";
+            this.ButtonNombre5.Size = new System.Drawing.Size(75, 23);
+            this.ButtonNombre5.TabIndex = 4;
+            this.ButtonNombre5.UseVisualStyleBackColor = true;
+            // 
+            // NombreCible
+            // 
+            this.NombreCible.AutoSize = true;
+            this.NombreCible.Location = new System.Drawing.Point(207, 134);
+            this.NombreCible.Name = "NombreCible";
+            this.NombreCible.Size = new System.Drawing.Size(13, 13);
+            this.NombreCible.TabIndex = 5;
+            this.NombreCible.Text = "0";
+            // 
+            // TextCible
+            // 
+            this.TextCible.AutoSize = true;
+            this.TextCible.Location = new System.Drawing.Point(206, 165);
+            this.TextCible.Name = "TextCible";
+            this.TextCible.Size = new System.Drawing.Size(30, 13);
+            this.TextCible.TabIndex = 6;
+            this.TextCible.Text = "Cible";
+            // 
+            // TextTimer
+            // 
+            this.TextTimer.AutoSize = true;
+            this.TextTimer.Location = new System.Drawing.Point(103, 134);
+            this.TextTimer.Name = "TextTimer";
+            this.TextTimer.Size = new System.Drawing.Size(49, 13);
+            this.TextTimer.TabIndex = 7;
+            this.TextTimer.Text = "00:00:00";
+            // 
+            // TextTemps
+            // 
+            this.TextTemps.AutoSize = true;
+            this.TextTemps.Location = new System.Drawing.Point(113, 165);
+            this.TextTemps.Name = "TextTemps";
+            this.TextTemps.Size = new System.Drawing.Size(39, 13);
+            this.TextTemps.TabIndex = 8;
+            this.TextTemps.Text = "Temps";
+            // 
+            // TextAffichageScore
+            // 
+            this.TextAffichageScore.AutoSize = true;
+            this.TextAffichageScore.Location = new System.Drawing.Point(319, 134);
+            this.TextAffichageScore.Name = "TextAffichageScore";
+            this.TextAffichageScore.Size = new System.Drawing.Size(13, 13);
+            this.TextAffichageScore.TabIndex = 9;
+            this.TextAffichageScore.Text = "0";
+            // 
+            // TextScore
+            // 
+            this.TextScore.AutoSize = true;
+            this.TextScore.Location = new System.Drawing.Point(306, 165);
+            this.TextScore.Name = "TextScore";
+            this.TextScore.Size = new System.Drawing.Size(35, 13);
+            this.TextScore.TabIndex = 10;
+            this.TextScore.Text = "Score";
+            // 
+            // ButtonSuivant
+            // 
+            this.ButtonSuivant.Location = new System.Drawing.Point(383, 160);
+            this.ButtonSuivant.Name = "ButtonSuivant";
+            this.ButtonSuivant.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSuivant.TabIndex = 11;
+            this.ButtonSuivant.Text = "Suivant";
+            this.ButtonSuivant.UseVisualStyleBackColor = true;
+            // 
+            // ButtonRetour
+            // 
+            this.ButtonRetour.Location = new System.Drawing.Point(12, 160);
+            this.ButtonRetour.Name = "ButtonRetour";
+            this.ButtonRetour.Size = new System.Drawing.Size(75, 23);
+            this.ButtonRetour.TabIndex = 12;
+            this.ButtonRetour.Text = "Retour";
+            this.ButtonRetour.UseVisualStyleBackColor = true;
+            // 
+            // ButtonPlus
+            // 
+            this.ButtonPlus.Location = new System.Drawing.Point(57, 280);
+            this.ButtonPlus.Name = "ButtonPlus";
+            this.ButtonPlus.Size = new System.Drawing.Size(75, 23);
+            this.ButtonPlus.TabIndex = 13;
+            this.ButtonPlus.Text = "+";
+            this.ButtonPlus.UseVisualStyleBackColor = true;
+            // 
+            // ButtonMoins
+            // 
+            this.ButtonMoins.Location = new System.Drawing.Point(145, 280);
+            this.ButtonMoins.Name = "ButtonMoins";
+            this.ButtonMoins.Size = new System.Drawing.Size(75, 23);
+            this.ButtonMoins.TabIndex = 14;
+            this.ButtonMoins.Text = "-";
+            this.ButtonMoins.UseVisualStyleBackColor = true;
+            // 
+            // ButtonFois
+            // 
+            this.ButtonFois.Location = new System.Drawing.Point(226, 280);
+            this.ButtonFois.Name = "ButtonFois";
+            this.ButtonFois.Size = new System.Drawing.Size(75, 23);
+            this.ButtonFois.TabIndex = 15;
+            this.ButtonFois.Text = "x";
+            this.ButtonFois.UseVisualStyleBackColor = true;
+            // 
+            // ButtonDiv
+            // 
+            this.ButtonDiv.Location = new System.Drawing.Point(313, 280);
+            this.ButtonDiv.Name = "ButtonDiv";
+            this.ButtonDiv.Size = new System.Drawing.Size(75, 23);
+            this.ButtonDiv.TabIndex = 16;
+            this.ButtonDiv.Text = "/";
+            this.ButtonDiv.UseVisualStyleBackColor = true;
+            // 
+            // MathadorGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 479);
+            this.Controls.Add(this.ButtonDiv);
+            this.Controls.Add(this.ButtonFois);
+            this.Controls.Add(this.ButtonMoins);
+            this.Controls.Add(this.ButtonPlus);
+            this.Controls.Add(this.ButtonRetour);
+            this.Controls.Add(this.ButtonSuivant);
+            this.Controls.Add(this.TextScore);
+            this.Controls.Add(this.TextAffichageScore);
+            this.Controls.Add(this.TextTemps);
+            this.Controls.Add(this.TextTimer);
+            this.Controls.Add(this.TextCible);
+            this.Controls.Add(this.NombreCible);
+            this.Controls.Add(this.ButtonNombre5);
+            this.Controls.Add(this.BouttonNombre4);
+            this.Controls.Add(this.BouttonNombre3);
+            this.Controls.Add(this.BouttonNombre2);
+            this.Controls.Add(this.BouttonNombre1);
+            this.Name = "MathadorGame";
+            this.Text = "MathadorGame";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BouttonNombre1;
+        private System.Windows.Forms.Button BouttonNombre2;
+        private System.Windows.Forms.Button BouttonNombre3;
+        private System.Windows.Forms.Button BouttonNombre4;
+        private System.Windows.Forms.Button ButtonNombre5;
+        private System.Windows.Forms.Label NombreCible;
+        private System.Windows.Forms.Label TextCible;
+        private System.Windows.Forms.Label TextTimer;
+        private System.Windows.Forms.Label TextTemps;
+        private System.Windows.Forms.Label TextAffichageScore;
+        private System.Windows.Forms.Label TextScore;
+        private System.Windows.Forms.Button ButtonSuivant;
+        private System.Windows.Forms.Button ButtonRetour;
+        private System.Windows.Forms.Button ButtonPlus;
+        private System.Windows.Forms.Button ButtonMoins;
+        private System.Windows.Forms.Button ButtonFois;
+        private System.Windows.Forms.Button ButtonDiv;
+    }
+}
+
